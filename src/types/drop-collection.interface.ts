cd409b23634dc420ce759ed0f529c1418e05db8b
jcurbelo <curbelorobin@gmail.com>
2022-04-17 12:56:11 -0400

    exposes wallet address in events

--- src/types/drop-collection.interface.ts
+++ src/types/drop-collection.interface.ts
@@ -2,6 +2,7 @@ export interface DropCollection {
   maxPerMint: number;
   maxPerWallet: number;
   walletTokensCount: number;
+  walletAddress: string;
   totalSupply: number;
   maxAmount: number;
 }
